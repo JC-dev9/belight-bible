1a5adc46e1ef026e8965337b927285ddaaf3d193
JC-dev9 <juanloza483@gmail.com>
2025-10-27 17:14:31 +0000

    adição da biblia em json

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,19 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 import 'screens/login_screen.dart';
 import 'screens/register_screen.dart';
 import 'screens/forgot_password.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 const supabaseUrl = 'https://dupoveauficvwgtrvxsx.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR1cG92ZWF1ZmljdndndHJ2eHN4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE0ODM2NTAsImV4cCI6MjA3NzA1OTY1MH0.9Gk_d6nj1cGSa6nbRHyuPmtiw7VU8Te7P8_qR5hRg8g';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR1cG92ZWF1ZmljdndndHJ2eHN4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE0ODM2NTAsImV4cCI6MjA3NzA1OTY1MH0.9Gk_d6nj1cGSa6nbRHyuPmtiw7VU8Te7P8_qR5hRg8g';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // Inicializa Supabase
+  // Inicializa Hive
+  await Hive.initFlutter();
+  await Hive.openBox('bibleBox'); // caixa para armazenar a Bíblia offline
+
+  // Inicializa Supabase (para login, registro, etc.)
   await Supabase.initialize(
     url: supabaseUrl,
     anonKey: supabaseKey,
@@ -27,8 +32,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Bible App',
-
-      // Tema claro
       theme: ThemeData(
         brightness: Brightness.light,
         primarySwatch: Colors.yellow,
@@ -38,8 +41,6 @@ class MyApp extends StatelessWidget {
         hintColor: Colors.grey.shade600,
         textTheme: const TextTheme(bodyMedium: TextStyle(color: Colors.black)),
       ),
-
-      // Tema escuro
       darkTheme: ThemeData(
         brightness: Brightness.dark,
         primarySwatch: Colors.yellow,
@@ -49,14 +50,8 @@ class MyApp extends StatelessWidget {
         hintColor: Colors.grey.shade400,
         textTheme: const TextTheme(bodyMedium: TextStyle(color: Colors.white)),
       ),
-
-      // Usa o tema do sistema automaticamente
       themeMode: ThemeMode.system,
-
-      // Rota inicial
       initialRoute: '/',
-
-      // Rotas da aplicação
       routes: {
         '/': (context) => const LoginScreen(),
         '/register': (context) => const RegisterScreen(),
